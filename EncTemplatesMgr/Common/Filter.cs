52225dedd723895652ec49239f12f97af830ca27
Aaron Crow <aaroncrowit@gmail.com>
2023-10-18 15:56:42 -0600

    Updated Filter.cs to enable requiring specific filter componenets match.

--- EncTemplatesMgr/Common/Filter.cs
+++ EncTemplatesMgr/Common/Filter.cs
@@ -1,18 +1,31 @@
 ﻿using EllieMae.EMLite.Common;
 using EllieMae.EMLite.DataEngine;
+using EllieMae.EMLite.DataEngine.Log;
 using EncTemplatesMgr.Helpers;
 using EncTemplatesMgr.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Windows.Media.Animation;
 
 namespace EncTemplatesMgr.Common
 {
     public class Filter
     {
         /// <summary>
+        /// Include all templates.
+        /// </summary>
+        public bool IncludeAllTemplates { get; set; }
+
+        /// <summary>
         /// Check if fileSystemEntry.Path contains this.
         /// </summary>
         public string FilterFilePath { get; set; }
+
+        /// <summary>
+        /// If true, file path must match.
+        /// </summary>
+        public bool FilePathRequired { get; set; }
 
         /// <summary>
         /// Check if fileSystemEntry.Name contains this.
@@ -20,9 +33,24 @@ namespace EncTemplatesMgr.Common
         public string FilterTemplateName { get; set; }
 
         /// <summary>
+        /// If true, template name must match.
+        /// </summary>
+        public bool TemplateNameRequired { get; set; }
+
+        /// <summary>
         /// Check if template data contains a match for a field with the given value.
         /// </summary>
         public Dictionary<string, string> FilterFieldValues { get; set; }
+
+        /// <summary>
+        /// If true, filed value must return a match. If false, any criteria can match..
+        /// </summary>
+        public bool FieldValuesRequired { get; set; }
+
+        /// <summary>
+        /// If true, all field values must match. If false, any field values can match.
+        /// </summary>
+        public bool AllFieldValuesRequired { get; set; }
 
         /// <summary>
         /// Filter the provided objects.
@@ -33,22 +61,42 @@ namespace EncTemplatesMgr.Common
         /// Filter templates based on provided data.
         /// </summary>
         /// <param name="customTemplate">Custom Template to check data against.</param>
-        /// <returns></returns>
+        /// <returns>Template match found.</returns>
         public bool TemplateInFilter(ICustomTemplate customTemplate)
         {
-            if (this.FilterFilePath == "*" || this.FilterTemplateName == "*")
+            var resultFound = false;
+            if (this.IncludeAllTemplates)
                 return true;
 
             if (this.FilterFilePath != string.Empty && customTemplate.FilePath.Contains(this.FilterFilePath))
-                return true;
+            {
+                resultFound = true;
+            }
+            else if (this.FilePathRequired)
+            {
+                return false;
+            }
 
             if (this.FilterTemplateName != string.Empty && customTemplate.Name.Contains(this.FilterTemplateName))
-                return true;
-
-            if (this.FilterFieldValues == null || this.FilterFieldValues.Count < 1)
+            {
+                resultFound = true;
+            }
+            else if (this.TemplateNameRequired)
+            {
                 return false;
+            }
 
-            return CheckTemplateData(customTemplate);
+            if ((this.FilterFieldValues != null && this.FilterFieldValues.Count > 0) &&
+                this.CheckTemplateData(customTemplate))
+            {
+                resultFound = true;
+            }
+            else if (this.FieldValuesRequired)
+            {
+                return false;
+            }
+
+            return resultFound;
         }
 
         /// <summary>
@@ -59,53 +107,83 @@ namespace EncTemplatesMgr.Common
         /// <returns>True if a match is found.</returns>
         public bool TemplateInFilter(FileSystemEntry fileSystemEntry, FieldDataTemplate fieldDataTemplate)
         {
-            if (this.FilterFilePath == "*" || this.FilterTemplateName == "*")
+            var resultFound = false;
+            if (this.IncludeAllTemplates)
                 return true;
 
             if (this.FilterFilePath != string.Empty && fileSystemEntry.Path.Contains(this.FilterFilePath))
-                return true;
+            {
+                resultFound = true;
+            }
+            else if (this.FilePathRequired)
+            {
+                return false;
+            }
 
             if (this.FilterTemplateName != string.Empty && fileSystemEntry.Name.Contains(this.FilterTemplateName))
-                return true;
-
-            if (this.FilterFieldValues == null || FilterFieldValues.Count < 1)
+            {
+                resultFound = true;
+            }
+            else if (this.TemplateNameRequired)
+            {
                 return false;
+            }
 
-            return CheckTemplateData(fieldDataTemplate);
-    
+            if ((this.FilterFieldValues != null && this.FilterFieldValues.Count > 0) &&
+                this.CheckTemplateData(fieldDataTemplate))
+            {
+                resultFound = true;
+            }
+            else if (this.FieldValuesRequired)
+            {
+                return false;
+            }
+
+            return resultFound;
         }
 
         private bool CheckTemplateData(ICustomTemplate customTemplate)
         {
+            var valueFound = false;
             foreach (var fieldValue in this.FilterFieldValues)
             {
                 try
                 {
                     if (customTemplate.FieldIDsAndValues[fieldValue.Key] != fieldValue.Value)
                     {
-                        return false;
+                        if (this.AllFieldValuesRequired)
+                            return false;
+                    }
+                    else
+                    {
+                        valueFound = true;
                     }
                 }
                 catch (Exception ex)
                 {
-                    Log.Logger.Verbose("CheckTemplateDetails " + ex.ToString());
+                    Log.Logger.Error($"CheckTemplateData Error comparing field: {fieldValue.Key}; value: {fieldValue.Value}; in {customTemplate.Name}. Exception: {ex}");
                     return false;
                 }
             }
 
-            return true;
+            return valueFound;
         }
 
         private bool CheckTemplateData(FieldDataTemplate fieldDataTemplate)
         {
-            // ToDo: this functions as an 'and' filter, consider implementing an 'or' filter as well.
+            var valueFound = false;
             foreach (var kvp in this.FilterFieldValues)
             {
                 try
                 {
                     if (fieldDataTemplate.GetField(kvp.Key) != kvp.Value)
                     {
-                        return false;
+                        if (this.AllFieldValuesRequired)
+                            return false;
+                    }
+                    else
+                    {
+                        valueFound = true;
                     }
                 }
                 catch (Exception ex)
@@ -115,7 +193,7 @@ namespace EncTemplatesMgr.Common
                 }
             }
 
-            return true;
+            return valueFound;
         }
     }
 }
